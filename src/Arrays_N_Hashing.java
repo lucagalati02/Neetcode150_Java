b8cfd90bde19dd1c1d9ac2b7e186de4c5e2deb39
Luca Galati <lucagalati31@gmail.com>
2025-05-22 10:59:29 -0400

    q2

--- src/Arrays_N_Hashing.java
+++ src/Arrays_N_Hashing.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Arrays_N_Hashing {
     public boolean hasDuplicate(int[] nums) {
         //https://neetcode.io/problems/duplicate-integer
-        //3 minutes
+        //3:00
         Arrays.sort(nums);
 
         for (int i = 1; i < nums.length; i++){
@@ -13,5 +13,17 @@ public class Arrays_N_Hashing {
             }
         }
         return false;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        //https://neetcode.io/problems/is-anagram
+        //3:31
+        char[] s_a = s.toCharArray();
+        char[] t_a = t.toCharArray();
+
+        Arrays.sort(s_a);
+        Arrays.sort(t_a);
+
+        return Arrays.equals(s_a, t_a);
     }
 }
