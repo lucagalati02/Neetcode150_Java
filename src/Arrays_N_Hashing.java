02092b7581d077c8db6b73956752c4f302ddabcb
Luca Galati <lucagalati31@gmail.com>
2025-05-22 11:13:32 -0400

    q3

--- src/Arrays_N_Hashing.java
+++ src/Arrays_N_Hashing.java
@@ -26,4 +26,23 @@ public class Arrays_N_Hashing {
 
         return Arrays.equals(s_a, t_a);
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        //https://neetcode.io/problems/two-integer-sum
+        //13:13
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = {};
+
+        for (int i = 0; i < nums.length; i++) {
+            int desire = target - nums[i];
+
+            if (map.containsKey(desire)) {
+                ans =  new int[] {map.get(desire), i};
+            }
+            else {
+                map.put(nums[i], i);
+            }
+        }
+        return ans;
+    }
 }
