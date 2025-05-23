1c2269732750df392c56f268819873c2fdf82a15
Luca Galati <lucagalati31@gmail.com>
2025-05-22 20:25:38 -0400

    q10 fixed

--- src/Stacks.java
+++ src/Stacks.java
@@ -3,23 +3,18 @@ import java.util.*;
 
 public class Stacks {
     public boolean isValid(String s) {
-        //https://neetcode.io/problems/validate-parentheses
-        //34:57
-        Map<Character,Character> map = new HashMap<>();
+        Map<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
-
-        Deque<Character> stack = new ArrayDeque<>();
+        Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
             if (map.containsKey(c)) {
-                // c is a closing bracket
-                if (stack.isEmpty() || stack.pop() != map.get(c)) {
+                if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
                     return false;
                 }
-            } else {
-                // c is (presumably) an opening bracket
+            } else if (map.containsValue(c)) {
                 stack.push(c);
             }
         }
