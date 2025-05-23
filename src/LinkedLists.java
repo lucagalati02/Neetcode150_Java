57653d39984ff9bc8ccade6a1ed55017e488430c
Luca Galati <lucagalati31@gmail.com>
2025-05-23 19:56:06 -0400

    q

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -16,6 +16,33 @@ public class LinkedLists {
         }
         return prev;
     }
+
+    ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //https://neetcode.io/problems/merge-two-sorted-linked-lists
+        //11:16
+        ListNode head = new ListNode(0);
+        ListNode node = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+
+        if (list1 != null) {
+            node.next = list1;
+        } else {
+            node.next = list2;
+        }
+
+        return head.next;
+    }
 }
 
 class ListNode {
