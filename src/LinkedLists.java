60e42cbe3368c3b57c0cce13717bdf78738ee433
Luca Galati <lucagalati31@gmail.com>
2025-05-23 19:37:15 -0400

    q

---
+++ src/LinkedLists.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class LinkedLists {
+    //https://neetcode.io/problems/reverse-a-linked-list
+    //44:13
+    ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
