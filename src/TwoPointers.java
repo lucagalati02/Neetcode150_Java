cef9d0bec9adf229e99d879de46ab31f5eeb061a
Luca Galati <lucagalati31@gmail.com>
2025-05-23 12:59:00 -0400

    q

--- src/TwoPointers.java
+++ src/TwoPointers.java
@@ -23,4 +23,62 @@ public class TwoPointers {
         }
         return true;
     }
+
+    public int[] twoSum(int[] numbers, int target) {
+        //https://neetcode.io/problems/two-integer-sum-ii
+        //5:25
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (numbers[left] + numbers[right] != target) {
+            int current = numbers[left] + numbers[right];
+            if (current < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return new int[] {left+1, right+1};
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        //https://neetcode.io/problems/three-integer-sum
+        //16:22
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null) return res;
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] > 0) break;
+            // skip duplicates for the “first” element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1, right = n - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // skip duplicates on left and right
+                    while (left < right && nums[left] == nums[left + 1])  left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                else if (sum < target) {
+                    left++;
+                }
+                else {  // sum > target
+                    right--;
+                }
+            }
+        }
+
+        return res;
+    }
 }
