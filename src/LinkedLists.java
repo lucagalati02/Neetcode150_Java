246d55735a1e02b87f88124c8d3e3cfe70eac715
Luca Galati <lucagalati31@gmail.com>
2025-05-25 10:55:34 -0400

    q

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -186,3 +186,74 @@ class Node {
         this.random = null;
     }
 }
+
+//{
+//https://neetcode.io/problems/lru-cache
+//47:39
+class LRUCache {
+
+    private class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+    private final Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+
+        // Dummy nodes to avoid null checks
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+
+        Node node = map.get(key);
+        remove(node);
+        insertToFront(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+            map.remove(key);
+        }
+
+        if (map.size() == capacity) {
+            Node lru = tail.prev;
+            remove(lru);
+            map.remove(lru.key);
+        }
+
+        Node newNode = new Node(key, value);
+        insertToFront(newNode);
+        map.put(key, newNode);
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertToFront(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+}
+//}
