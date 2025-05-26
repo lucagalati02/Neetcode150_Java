6f71ad506a8fb329e6cb6f761887b9db8c3c3e64
Luca Galati <lucagalati31@gmail.com>
2025-05-26 09:34:48 -0400

    q

--- src/BinarySearching.java
+++ src/BinarySearching.java
@@ -20,4 +20,48 @@ public class BinarySearching {
         }
         return -1;
     }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //https://neetcode.io/problems/search-2d-matrix
+        //37:23
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        while (top <= bottom) {
+            int mid = top + (bottom - top) / 2;
+            if (target < matrix[mid][0]) {
+                bottom = mid - 1;
+            }
+            else if (target > matrix[mid][0]) {
+                top = mid + 1;
+            }
+            else {
+                return true;
+            }
+        }
+
+        if (bottom < 0) {
+            return false;
+        }
+        int row = bottom;
+
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target < matrix[row][mid]) {
+                right = mid - 1;
+            } else if (target > matrix[row][mid]) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
