5e2c823be3e6ea1ee12558609e242600b70e3873
Luca Galati <lucagalati31@gmail.com>
2025-05-26 10:43:24 -0400

    q

---
+++ src/Trees.java
@@ -0,0 +1,31 @@
+public class Trees {
+    TreeNode invertTree(TreeNode root) {
+        //https://neetcode.io/problems/invert-a-binary-tree
+        //6:17
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
