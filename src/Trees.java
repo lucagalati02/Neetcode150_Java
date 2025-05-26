a64be5c429925e9a2dd05bcb082e6593398765f3
Luca Galati <lucagalati31@gmail.com>
2025-05-26 11:25:54 -0400

    q

--- src/Trees.java
+++ src/Trees.java
@@ -25,6 +25,25 @@ public class Trees {
 
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
+
+    //{
+    //https://neetcode.io/problems/binary-tree-diameter
+    //8:23
+    int diameterOfBinaryTree(TreeNode root) {
+        int[] res = new int[1];
+        dfs(root, res);
+        return res[0];
+    }
+    int dfs(TreeNode root, int[] res) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left, res);
+        int right = dfs(root.right, res);
+        res[0] = Math.max(res[0], left + right);
+        return 1 + Math.max(left, right);
+    }
+    //}
 }
 
 class TreeNode {
